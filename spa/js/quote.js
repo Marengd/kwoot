4b316cf1cd3c48f38aef1fa8512b4e62ae644ff7
marengd <danian.marengo@hva.nl>
2023-02-23 22:19:05 +0100

    Quote.js 🤓 aanpassing

--- spa/js/quote.js
+++ spa/js/quote.js
@@ -1,27 +1,31 @@
-function $ (element) {
+function $ (element) { // Helper Function
   return document.querySelector(element);
 }
 
 // Logic
-async function getQuotes(wisdomtext) {
+async function getQuotes(quoteTextfield) {
   const response = await fetch('https://api.kanye.rest');
   const data = await response.json();
   const quote = data.quote;
 
   console.log(data);
 
-  if (wisdomtext !== null) {
-    wisdomtext.innerHTML = `"${quote}"`;
+  if (quoteTextfield !== null) { // Checks If QuoteTextfield Is Not Null
+    quoteTextfield.innerHTML = `"${quote}"`;
   }
 }
 
-// Event listener using event delegation
+// Event listener
 document.addEventListener('click', function(event) {
-  if (event.target.matches('#quoteButton')) {
+  const quoteButton = $('#quoteButton');
+  const quoteTextfield = $('#quote');
+
+  if (event.target === quoteButton) { // Checks If A Click Occurred On The Element QuoteButton
     console.log("Button clicked");
-    getQuotes($('#quote'));
+    getQuotes(quoteTextfield); // JavaScript Closure
   }
 });
 
-// Call getQuotes on page load
+
+// Call getQuotes On Page Load
 getQuotes($('#quote'));
